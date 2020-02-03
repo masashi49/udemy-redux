d94757bc9346bea88ab69c7958bfec2322b7c241
山田将史 <13416062+masashi49@users.noreply.github.com>
2020-02-04 00:16:11 +0900

    classに変更

--- test/src/App.js
+++ test/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App () {
-  return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <h1 className="App">
+        hello testasfasdf
+    </h1>
+    )
+  };
 }
 
 export default App;
