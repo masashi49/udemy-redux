ede1b89465875c5c4781f95eee15ac216270de33
yamada <yamada@reas.jp>
2020-02-05 09:28:22 +0900

    onclickなどで遊ぶ

--- test/src/App.js
+++ test/src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render () {
-    return (
+// class App extends Component {
+//   render () {
+//     return (
+//       <h1 className="App">
+//         hello testasfasdf
+//     </h1>
+//     )
+//   };
+// }
+
+function App() {
+
+  let i = 0;
+  let v = 0;
+
+  return (
+    <React.Fragment>
       <h1 className="App">
-        hello testasfasdf
-    </h1>
-    )
-  };
+        hello testasfasdf ss
+      </h1>
+      <label htmlFor="text_area">文字を入力</label>
+      <input id="text_area" type="text" onChange={() => console.log(i++)} /> <br />
+      <button onClick={() => console.log(v++)} >console.logが増える</button>
+    </React.Fragment >
+  )
 }
 
 export default App;
