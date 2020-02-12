d551dba1a069aa9b45d35aa2443eedf4b28b7bf5
山田将史 <13416062+masashi49@users.noreply.github.com>
2020-02-12 12:52:17 +0900

    no message

--- test/src/App.js
+++ test/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { element } from 'prop-types';
 
 class App extends Component {
   render () {
+    const greeeiing = 'hiiiiiiiiiii'
+    const dom = <span>{ greeeiing }</span>
+
     return (
-      <h1 className="App">
-        hello testasfasdf
-    </h1>
+      <React.Fragment>
+        <h1 className="App is_first">
+          hello testasfasdf { dom }
+        </h1>
+        <button onClick={ () => { console.log( 100 ) } }>アロー関数でonclickイベントを埋め込める</button>
+
+        <label htmlFor="testInput">てst</label>
+        <input id="testInput" onChange={ () => { console.log( 123243 ) } } />
+      </React.Fragment>
     )
   };
 }
